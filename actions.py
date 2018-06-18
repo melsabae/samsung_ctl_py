f1be3382270ab20617958ba3b5e84a45b42df9b6
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2018-06-18 07:54:02 -0500

    for whatever reason the cpu control values are going into the usb file, have not tested the other way

--- actions.py
+++ actions.py
@@ -4,9 +4,16 @@ import io
 from sys import argv, exc_info
 
 
+"""
+All functions defined herein operate by contract
+"""
+
+
 _filepaths = {
-        "performance_level": "/sys/devices/platform/samsung/performance_level",
-        "usb_charge": "/sys/devices/platform/samsung/usb_charge",
+        #"performance_level": "/sys/devices/platform/samsung/performance_level",
+        #"usb_charge": "/sys/devices/platform/samsung/usb_charge",
+        "performance_level": "./cpu",
+        "usb_charge": "./usb"
         }
 
 _controls = {
@@ -14,56 +21,36 @@ _controls = {
         "usb_charge": ["0", "1"],
         }
 
-_ux = {
+ux = {
         "cpu": "performance_level",
-        "usb": "usb_charge"
+        "usb": "usb_charge",
         }
 
 
 def get_file_path(c: str) -> str:
-    return _filepaths[c]
+    return _filepaths[ux[c]]
 
 
 def get_control_values(c: str) -> [str]:
-    return _controls[c]
+    return _controls[ux[c]]
 
 
 def get_next_in_cycle(l: [str], c: str) -> str:
-    return l[(l.index(c) + 1) % len(l)] if c in l else None
+    return l[(l.index(c) + 1) % len(l)]
 
 
 def get_current_value(c: str) -> str:
     return io.open(get_file_path(c), "r").readline().strip()
 
 
-def write_new_value(fp: str, v: str) -> bool:
+def update_value(c: str, v: str) -> bool:
+    fp = get_file_path(c)
     return io.open(fp, "w").write(v) == len(v)
 
 
 def cycle_value(c: str) -> bool:
-    fp = get_file_path(c)
     cv = get_control_values(c)
     curr = get_current_value(c)
     n = get_next_in_cycle(cv, curr)
-    return write_new_value(fp, n)
+    return update_value(c, n)
 
-
-#def main(i: str):
-#    r = False
-#    try:
-#        r = cycle_value(i)
-#    except:
-#        pass
-#    finally:
-#        if r:
-#            print("{} updated to {}".format(i, get_current_value(i)))
-#
-#        if not r:
-#            print("{} unchanged (permissions error?)".format(i))
-
-
-if __name__ == "__main__":
-    if len(argv) > 1 and argv[1] in _ux:
-        main(_ux[argv[1]])
-    else:
-        print("usage: {} {}".format(argv[0], list(_ux)))
--- daemon.py
+++ daemon.py
@@ -6,9 +6,33 @@ import sys
 import os
 import logging
 import signal
-
+import actions as sa
 
 logger = print
+
+tal = list(
+        (("set", _, __, sa.get_file_path(_)), lambda: sa.update_value(_, __)) for _ in sa.ux for __ in sa.get_control_values(_)
+        )
+ta = tal[0]
+print(ta, sa.get_file_path(ta[0][1]), ta[1]())
+sys.exit(0)
+
+ga = list(zip([("get", _) for _ in sa.ux], [lambda: sa.get_current_value(_) for _ in sa.ux]))
+ca = list(zip([("cyc", _) for _ in sa.ux], [lambda: sa.cycle_value(_) for _ in sa.ux]))
+sa = [(("set", _, __), lambda: sa.update_value(_, __)) for _ in sa.ux for __ in sa.get_control_values(_)]
+am = { k: v for (k,v) in ga + ca + sa }
+
+get_actions = {
+        ("get", _): (lambda: sa.get_current_value(_)) for _ in sa.ux
+        }
+
+cyc_actions = {
+        ("cyc", _): (lambda: sa.cycle_value(_)) for _ in sa.ux
+        }
+
+set_actions = {
+        ("set", _, __): (lambda: sa.update_value(_, __)) for _ in sa.ux for __ in sa.get_control_values(_)
+        }
 
 
 def sig_handler(signum, frame):
@@ -32,6 +56,10 @@ def main():
 
     while True:
         conn, _ = sock.accept()
+
+        """
+        Command format: {get, set, cyc} {_ in ux} ["set" parameter]
+        """
 
         try:
             data = conn.recv(4096)
