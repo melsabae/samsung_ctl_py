3bdf0a3f32f66482504ed5f2c4b24b4ea78bc12b
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2019-08-10 16:12:38 -0400

    maybe i should rewrite some things

--- daemon.py
+++ daemon.py
@@ -11,61 +11,73 @@ import actions as act
 from itertools import chain
 
 
-logger = print
+def global_cleanup():
+    global _serv
+    global _sock
+
+    _sock.close()
+    os.unlink(_serv) if os.path.exists(_serv) else {}
 
 
-"""
-Command format: {get, set, cyc} {_ in ux} ["set" parameter]
-"""
-am = {
-        k:v for (k, v) in chain(
-            ((("get", _), p(act.get_current_value, c=_)) for _ in act.ux)
-            , ((("cyc", _), p(act.cycle_value, c=_)) for _ in act.ux)
-            , ((("set", _, __), p(act.update_value, c=_, v=__)) for _ in act.ux for __ in act.get_control_values(_))
-        )
-}
-
-
-def get_action(i):
-    return am[i] if i in am else lambda: "NACK"
+def get_action(m, i):
+    return m[i] if i in m else lambda: "NACK"
 
 
 def sig_handler(signum, frame):
-    global sock
-    sock.close()
+    global _sock
+    global _serv
+    global logger
+
     logger("{} {}".format("received signal", signum))
+    global_cleanup()
     sys.exit(0)
 
 
 def main():
-    global sock
-    global am
+    global _sock
+    global _serv
+    global _am
 
-    serv = "./samsung_ctl"
-    os.unlink(serv) if os.path.exists(serv) else {}
+    os.unlink(_serv) if os.path.exists(_serv) else {}
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    prev_mask = os.umask(0o000) # set permissions for socket
-    sock.bind(serv)
-    os.umask(prev_mask)
+    prev_mask = os.umask(0o000)  # set permissions for socket
+    _sock.bind(_serv)
+    os.umask(prev_mask)  # restore permissions
 
-    sock.listen()
+    _sock.listen()
 
     while True:
-        conn, _ = sock.accept()
+        conn, _ = _sock.accept()
         try:
             data = conn.recv(4096)
             c = tuple(data.decode('utf-8').split())
-            res = get_action(c)()
+            res = get_action(_am, c)()
             conn.send(bytes(res.encode('utf-8')))
             logger("recv: {}, send: {}".format(repr(c), repr(res)))
         finally:
             conn.close()
 
 
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+
+_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+_serv = "./samsung_ctl"
+logger = print
+
+
+"""
+Command format: {get, set, cyc} {_ in ux} ["set" parameter]
+"""
+_am = {
+        k:v for k, v in chain(
+            ((("get", _), p(act.get_current_value, c=_)) for _ in act.ux)
+            , ((("cyc", _), p(act.cycle_value, c=_)) for _ in act.ux)
+            , ((("set", _, __), p(act.update_value, c=_, v=__)) for _ in act.ux for __ in act.get_control_values(_))
+        )
+}
 if __name__ == "__main__":
     main()
 
